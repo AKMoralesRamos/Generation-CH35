95d0bdc6319b7fd7c87ad4198d26fd5d764785d4
AKMoralesRamos <dental.morak@gmail.com>
2023-12-07 20:42:38 -0600

    cambio carpetas

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,16 +5,16 @@
 /* var nombre = prompt("Ingresa tu nombre");
 alert("Hola, " + nombre + " bienvenido a la CH35"); */
 
-var numero1 = parseInt (prompt("Ingresa el primer número: "));
+/* var numero1 = parseInt (prompt("Ingresa el primer número: "));
 var numero2 = parseInt (prompt("Ingresa el segundo número: "));
 console.log("la suma de ambos numeros es: " + (numero1 + numero2));
 console.log("la resta de ambos numeros es: " + (numero1 - numero2));
 console.log("la multiplicación de ambos numeros es: "+ (numero1 * numero2));
-console.log("la división de ambos numeros es: " + (numero1 / numero2));
+console.log("la división de ambos numeros es: " + (numero1 / numero2)); */
 
-/* var edad = prompt("Ingresa tu edad:");
+var edad = prompt("Ingresa tu edad:");
 if(edad >= 18) {
     alert("Eres mayor de edad")
 } else if( edad < 18) {
     alert("Eres menor de edad")
-}; */
+};

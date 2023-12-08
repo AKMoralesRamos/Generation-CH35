e28a3c206871248d9ddf6670a13c85ed339a135e
AKMoralesRamos <dental.morak@gmail.com>
2023-12-07 19:02:49 -0600

    css, portafolio práctica, bootstrap

---
+++ Javascript/script.js
@@ -0,0 +1,20 @@
+// comentario de una sola línea
+/* comentario multilinea */
+/* alert("hola mundo"); */
+
+/* var nombre = prompt("Ingresa tu nombre");
+alert("Hola, " + nombre + " bienvenido a la CH35"); */
+
+var numero1 = parseInt (prompt("Ingresa el primer número: "));
+var numero2 = parseInt (prompt("Ingresa el segundo número: "));
+console.log("la suma de ambos numeros es: " + (numero1 + numero2));
+console.log("la resta de ambos numeros es: " + (numero1 - numero2));
+console.log("la multiplicación de ambos numeros es: "+ (numero1 * numero2));
+console.log("la división de ambos numeros es: " + (numero1 / numero2));
+
+/* var edad = prompt("Ingresa tu edad:");
+if(edad >= 18) {
+    alert("Eres mayor de edad")
+} else if( edad < 18) {
+    alert("Eres menor de edad")
+}; */

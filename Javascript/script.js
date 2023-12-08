19064dfc6b8e7745afeb85ee56aef63c2a0c1a52
AKMoralesRamos <dental.morak@gmail.com>
2023-12-08 14:12:50 -0600

    tipos de datos en javascript

--- Javascript/script.js
+++ Javascript/script.js
@@ -12,9 +12,24 @@ console.log("la resta de ambos numeros es: " + (numero1 - numero2));
 console.log("la multiplicación de ambos numeros es: "+ (numero1 * numero2));
 console.log("la división de ambos numeros es: " + (numero1 / numero2)); */
 
-var edad = prompt("Ingresa tu edad:");
+/* var edad = prompt("Ingresa tu edad:");
 if(edad >= 18) {
     alert("Eres mayor de edad")
 } else if( edad < 18) {
     alert("Eres menor de edad")
-};
+}; */
+
+// Tipos de datos y variables
+// var, let y const la diferencia es el alcance var tiene un alcance global, let tienen alcance local y const se mantiene sin ningun cambio 
+/* String; cadena de texto (lleva comillas)
+number: numero
+booleana: true/false
+null:nulo
+undefined:no esta asigando o definido*/
+
+let nombre = "Daniel";
+let invitadosExtras = 2; 
+let esMayorEdad = true;
+let edad = 24;
+let invitadoEspecial = null;
+let horaDesalida= undefined;

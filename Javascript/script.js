e86f992cecddf040ccaffa37461e31af870e18b0
AKMoralesRamos <dental.morak@gmail.com>
2023-12-11 23:56:19 -0600

    receta

--- Javascript/script.js
+++ Javascript/script.js
@@ -93,7 +93,34 @@ console.log(typeof(huevo)); */
 console.log(huevoToString);
 console.log(typeof(huevoToString)); */
 
+/*--------------------RECETA DE COCINA-------------------*/
 
+let harina = parseInt ("150");
+let huevo = toString(1);
+let mantequilla = parseInt("5");
+let leche = parseInt("500");
+
+let nutella = 50;
+let nutellatoString = nutella.toString();
+console.log(typeof(nutellatoString));
+console.log(nutella);
+//console.log(typeof (toString(nutella)));
+
+/*console.log(typeof(harina));
+console.log(typeof(huevo)); */
+
+let pasoUno = "Paso 1: Vierte todos los ingredientes en la licuadora y mezcla hasta lograr una consistencia homogenea. ";
+let pasoDos = "<br> Paso 2: Despues, caliente un sarten con un poco de mantequilla. ";
+let pasoTres = "<br> Paso 3: Posteriormente, sirve la mezcla de masa para jokeis al centro del sarten dejandola caer suavemente. ";
+let pasoCuatro = "<br> Paso 4: Manten a flama baja y voltea una vez que aparezcan burbujas de aire. ";
+let pasoCinco = "<br> Paso 5: Ahora, repite agregando mantequilla al sarten cada vez que sea necesario.";
+let pasoSeis = "<br> Paso 6: Finalmente, sirve y disfruta.";
+
+let texto = pasoUno + pasoDos + pasoTres + pasoCuatro + pasoCinco + pasoSeis
+document.write(texto);
+
+textoDos = "Esto es " + nutella;
+document.write(textoDos);
 
 
 /*---------------- Sesión con Daniel Maldonado------------ */
@@ -135,3 +162,4 @@ console.log(employee);
 console.table(cars);
 //Mostrando mi objeto como table
 console.table(employee);
+
